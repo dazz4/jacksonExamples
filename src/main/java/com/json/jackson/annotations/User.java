c9283edf5b8eea309f42c72609cdde9f194b5775
Dazz41911 <dazz41911@gmail.com>
2019-07-11 10:26:47 +0100

    Jackson Annotations Examples

--- src/main/java/com/json/jackson/annotations/User.java
+++ src/main/java/com/json/jackson/annotations/User.java
@@ -1,4 +1,25 @@
 package com.json.jackson.annotations;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Component
 public class User {
+    @Id
+    @GeneratedValue
+    @JsonIgnore
+    private Long id;
+    private String name;
+    private String email;
 }
