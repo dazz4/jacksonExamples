137b2d8c02c330b8cdf91fab6b5965bb7495c82a
Dazz41911 <dazz41911@gmail.com>
2019-07-11 09:17:21 +0100

    JsonParser example

---
+++ src/main/java/com/json/jackson/jasonparser/Customer.java
@@ -0,0 +1,17 @@
+package com.json.jackson.jasonparser;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Component
+public class Customer {
+    private String name;
+    private String email;
+}
---
+++ src/test/java/com/json/jackson/jsonparser/CustomerTestSuite.java
@@ -0,0 +1,72 @@
+package com.json.jackson.jsonparser;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.json.jackson.jasonparser.Customer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerTestSuite {
+    public static final String JSON = "{\"name\":\"Dariusz Kaminski\", \"email\":\"kaminski.darek@icloud.com\"}";
+
+    @Autowired
+    private Customer customer;
+
+    @Test
+    public void testJsonParser() throws IOException {
+        JsonFactory factory = new JsonFactory();
+        try {
+            JsonParser parser = factory.createParser(JSON);
+
+            while (!parser.isClosed()) {
+                JsonToken jsonToken = parser.nextToken();
+                System.out.println("jsonToken =" + jsonToken);
+            }
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testJsonParserEquals() throws IOException {
+        JsonFactory factory = new JsonFactory();
+        try {
+            JsonParser parser = factory.createParser(JSON);
+
+            while (!parser.isClosed()) {
+                JsonToken jsonToken = parser.nextToken();
+
+                if (JsonToken.FIELD_NAME.equals(jsonToken)) {
+                    String fieldName = parser.getCurrentName();
+                    System.out.println(fieldName);
+
+                    jsonToken = parser.nextToken();
+
+                    if ("name".equals(fieldName)) {
+                        customer.setName(parser.getValueAsString());
+                    } else if ("email".equals(fieldName)) {
+                        customer.setEmail(parser.getValueAsString());
+                    }
+                }
+
+            }
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals("Dariusz Kaminski", customer.getName());
+        assertEquals("kaminski.darek@icloud.com", customer.getEmail());
+    }
+}
